d39597fa983b8acdfca937702c9d2423c2887bc7
evelynlucas <evelynlucas@pursuit.org>
2019-08-09 16:17:42 -0400

    Added RetrofitSingleton and UserService

--- app/src/main/java/org/pursuit/geniusplazaproficiencytest/MainActivity.java
+++ app/src/main/java/org/pursuit/geniusplazaproficiencytest/MainActivity.java
@@ -2,6 +2,15 @@ package org.pursuit.geniusplazaproficiencytest;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.pursuit.geniusplazaproficiencytest.model.Users;
+import org.pursuit.geniusplazaproficiencytest.network.RetrofitSingleton;
+import org.pursuit.geniusplazaproficiencytest.network.UserService;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RetrofitSingleton.getInstance()
+                .create(UserService.class)
+                .getUserData()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Users>() {
+                    @Override
+                    public void accept(Users users) throws Exception {
+                        Log.d("Evelyn", users.getData().get(0).getEmail());
+
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+
+                    }
+                });
     }
 }
